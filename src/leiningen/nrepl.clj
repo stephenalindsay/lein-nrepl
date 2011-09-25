1f0dd7d4075c47a3a330a02e659fd61aa62ecd8b
Steve Lindsay <stephen.a.lindsay@gmail.com>
2011-09-25 23:44:14 +1000

    Open up a local repl to the nrepl we've just started

--- src/leiningen/nrepl.clj
+++ src/leiningen/nrepl.clj
@@ -8,12 +8,29 @@
      (eval-in-project
        project
        `(~'do
-            (~'println "Starting nREPL on port:" ~p )
-            (~'clojure.tools.nrepl/start-server ~p)
-            (~'println "Running.")
+            (println "Starting nREPL on port:" ~p )
+            (let [[ss# _#] (~'clojure.tools.nrepl/start-server ~p)]
+              (~'println "Running.")
+              (let [connection# (~'clojure.tools.nrepl/connect "localhost" (.getLocalPort ss#))
+                    prompt#     #(print (str % "=> "))
+                    err#        print
+                    out#        print
+                    value#      print ]
+                (println "network-repl")
+                (println (str "Clojure " (clojure-version)))
+                (loop [ns# "user"]
+                  (prompt# ns#)
+                  (flush)
+                  (recur (last
+                           (for [res# (clojure.tools.nrepl/response-seq ((:send connection#) (pr-str (read))))]
+                             (do
+                               (when (:value res#) (value# (:value res#)))
+                               (when (:out res#) (out# (:out res#)))
+                               (when (:err res#) (err# (:err res#)))
+                               (:ns res#))))))))
             (~'Thread/sleep Long/MAX_VALUE))
        nil
        nil
-       `(require 'clojure.tools.nrepl)
+       '(require 'clojure.tools.nrepl)
        )))
   ([project] (nrepl project 11011)))
